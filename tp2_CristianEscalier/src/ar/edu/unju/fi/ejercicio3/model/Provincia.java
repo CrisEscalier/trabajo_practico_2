686cbc60401c820bfdab88076c59890b89f8d194
Cristian <Cristian@DESKTOP-PQFQ4PO>
2024-04-26 17:19:38 -0300

    Envio del paquete model y el enum Provincia del ejercicio 3 del tp2.

---
+++ tp2_CristianEscalier/src/ar/edu/unju/fi/ejercicio3/model/Provincia.java
@@ -0,0 +1,47 @@
+package ar.edu.unju.fi.ejercicio3.model;
+
+public enum Provincia {
+	//PROVICNIA(POBLACION,SUPERFICIE EN KM2)
+	//Datos poblacionales del censo de 2022
+	//https://es.wikipedia.org/wiki/Provincias_de_Argentina
+	JUJUY(811_611, 53_219), 
+    SALTA(1_441_351, 155_488),
+    TUCUMAN(1_731_820, 22_524), 
+    CATAMARCA(429_562, 102_602),
+    LA_RIOJA(383_865, 89_680),
+    SANTIAGO_DEL_ESTERO(1_060_906, 136_351);
+
+    private int cantidadPoblacion;
+    private int superficie;
+
+    private Provincia(int cantidadPoblacion, int superficie) {
+        this.cantidadPoblacion = cantidadPoblacion;
+        this.superficie = superficie;
+    }
+
+    public int getCantidadPoblacion() {
+        return cantidadPoblacion;
+    }
+
+    public void setCantidadPoblacion(int cantidadPoblacion) {
+        this.cantidadPoblacion = cantidadPoblacion;
+    }
+
+    public int getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(int superficie) {
+        this.superficie = superficie;
+    }
+
+    /**
+     * Metodo que calcula la densidad poblacional
+     * @return la densidad poblacional
+     */
+    public double calcularDensidadPoblacional() {
+        return (double) cantidadPoblacion / superficie;
+    }
+	
+	
+}
